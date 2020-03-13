5d3359de5e1058ece06dfd347f370e33f459546c
MantasMikal <mikalauskas.it@gmail.com>
2020-03-13 18:28:55 +0000

    fix(site): Attempt to fix imports #5

--- web/src/components/Primitive/Video/index.jsx
+++ web/src/components/Primitive/Video/index.jsx
@@ -56,3 +56,81 @@ Video.propTypes = {
 }
 
 export default Video
+
+
+////
+
+
+export const YouTubeEmbed = ({ hideControls, start, videoId, width, height, autoPlay, autoHide, mute, hideInfo }) => {
+  const srcPrefix = 'https://www.youtube.com/embed/'
+  const query = {
+    rel: 0, // https://developers.google.com/youtube/player_parameters#release_notes_08_23_2018
+    modestbranding: 1,
+    playsinline: 1,
+    ...(autoHide && { autoHide: 1 }),
+    ...(mute && { mute: 1 }),
+    ...(hideInfo && { showInfo: 0 }),
+    ...(hideControls && { controls: 0 }),
+    ...(autoPlay && { autoPlay: 1, }),
+    ...(start && { start })
+  }
+
+  return (
+    <iframe
+      title="Embedded YouTube video"
+      src={`${srcPrefix}${videoId}?${shallowObjectToQuery(query)}`}
+      width={width ? width : "560"}
+      height={height ? height : "315"}
+      frameBorder="0"
+      allowFullScreen
+      SameSite='None'
+      Secure
+    />
+  )
+}
+
+YouTubeEmbed.propTypes = {
+  hideControls: bool,
+  start: string,
+  videoId: string.isRequired,
+  width: string,
+  height: string,
+  autoPlay: bool,
+  autoHide: bool,
+  mute: bool,
+  showInfo: bool
+}
+
+
+
+//// 
+
+
+export const VimeoEmbed = ({ color, hideByline, hideTitle, start, videoId }) => {
+  const srcPrefix = 'https://player.vimeo.com/video/'
+  const query = {
+    ...(color && { color: color.replace('#', '') }),
+    ...(hideByline && { byline: 0 }),
+    ...(hideTitle && { title: 0 })
+  }
+  const formattedStart = start ? `#t=${start}s` : ``
+
+  return (
+    <iframe
+      title="Embedded YouTube video"
+      src={`${srcPrefix}${videoId}?${shallowObjectToQuery(query)}${formattedStart}`}
+      width="560"
+      height="315"
+      frameBorder="0"
+      allowFullScreen
+    />
+  )
+}
+
+VimeoEmbed.propTypes = {
+  start: string,
+  color: string,
+  hideTitle: bool,
+  hideByline: bool,
+  videoId: string.isRequired
+}
