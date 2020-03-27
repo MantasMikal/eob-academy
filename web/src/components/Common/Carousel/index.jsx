63da2e0b71153195e3d01305cefd917db40e11eb
MantasMikal <mikalauskas.it@gmail.com>
2020-03-27 00:08:33 +0000

    feat(site): Lazy load carousel

--- web/src/components/Common/Carousel/index.jsx
+++ web/src/components/Common/Carousel/index.jsx
@@ -6,6 +6,7 @@ import ButtonBase from 'Primitive/ButtonBase'
 import Icon from 'Primitive/Icon'
 
 import styles from './Carousel.module.scss'
+import LazyLoader from 'lib/lazy-loader/lazyLoader'
 
 function Carousel({ children }) {
   const isTablet = useMedia('(max-width: 960px)')
@@ -14,44 +15,46 @@ function Carousel({ children }) {
 
   return (
     <div className={styles.Root}>
-      <NukaCarousel
-        slidesToShow={slidesToShow}
-        cellSpacing={24}
-        enableKeyboardControls
-        frameOverflow="visible"
-        initialSlideHeight={360}
-        inirialSlideWidth={640}
-        speed={250}
-        transitionMode="scroll"
-        renderTopRightControls={({ nextSlide }) => (
-          <ButtonBase className={styles.ControlRight} onClick={nextSlide}>
-            <Icon
-              className={styles.ControlIcon}
-              type="chevron-right"
-              width={30}
-              height={20}
-              a11yText="Next slide"
-            />
-          </ButtonBase>
-        )}
-        renderTopLeftControls={({ previousSlide }) => (
-          <ButtonBase className={styles.ControlLeft} onClick={previousSlide}>
-            <Icon
-              className={styles.ControlIcon}
-              type="chevron-left"
-              width={30}
-              height={20}
-              a11yText="Previous Slide"
-            />
-          </ButtonBase>
-        )}
-        // Removes default controls
-        renderBottomCenterControls={() => null}
-        renderCenterRightControls={() => null}
-        renderCenterLeftControls={() => null}
-      >
-        {children}
-      </NukaCarousel>
+      <LazyLoader>
+        <NukaCarousel
+          slidesToShow={slidesToShow}
+          cellSpacing={24}
+          enableKeyboardControls
+          frameOverflow="visible"
+          initialSlideHeight={360}
+          inirialSlideWidth={640}
+          speed={250}
+          transitionMode="scroll"
+          renderTopRightControls={({ nextSlide }) => (
+            <ButtonBase className={styles.ControlRight} onClick={nextSlide}>
+              <Icon
+                className={styles.ControlIcon}
+                type="chevron-right"
+                width={30}
+                height={20}
+                a11yText="Next slide"
+              />
+            </ButtonBase>
+          )}
+          renderTopLeftControls={({ previousSlide }) => (
+            <ButtonBase className={styles.ControlLeft} onClick={previousSlide}>
+              <Icon
+                className={styles.ControlIcon}
+                type="chevron-left"
+                width={30}
+                height={20}
+                a11yText="Previous Slide"
+              />
+            </ButtonBase>
+          )}
+          // Removes default controls
+          renderBottomCenterControls={() => null}
+          renderCenterRightControls={() => null}
+          renderCenterLeftControls={() => null}
+        >
+          {children}
+        </NukaCarousel>
+      </LazyLoader>
     </div>
   )
 }
