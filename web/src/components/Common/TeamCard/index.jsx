fa6a69aef3e34dc0f5e9f9b0668f61b1ea235c07
MantasMikal <mikalauskas.it@gmail.com>
2020-04-26 15:35:09 +0100

    fix(site:TeamCard): Adjusts button size

--- web/src/components/Common/TeamCard/index.jsx
+++ web/src/components/Common/TeamCard/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import  { string } from 'prop-types'
+import { string } from 'prop-types'
 import { useDarkContext } from 'Context/DarkContext'
 import { cn } from 'lib/helpers'
 
@@ -11,26 +11,23 @@ import ButtonStandard from 'Primitive/ButtonStandard'
 
 import styles from './TeamCard.module.scss'
 
-
 // TODO
 // Replace Icon with image if using CMS
 
-const TeamCard = ({ logo, title, description, slug}) => {
+const TeamCard = ({ logo, title, description, slug }) => {
   const isDark = useDarkContext()
   return (
     <div className={cn(styles.TeamCard, isDark && styles.isDark)}>
       <Media media={logo} />
-      <Type className={styles.Title} size="titleLarge">
+      <Type className={styles.Title} size='titleLarge'>
         {title}
       </Type>
-      <Type className={styles.Description} size="base">
+      <Type className={styles.Description} size='base'>
         {description}
       </Type>
-      <SmartLink className={styles.MoreBtn} to={`/team/${slug.current}`}>
-        <ButtonStandard>
-          <Type size='base'>
-            READ MORE
-          </Type>
+      <SmartLink to={`/team/${slug.current}`}>
+        <ButtonStandard className={styles.MoreBtn}>
+          <Type size='base' demi>READ MORE</Type>
         </ButtonStandard>
       </SmartLink>
     </div>
