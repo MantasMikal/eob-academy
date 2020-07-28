760cb41ad0e6fc0c1c6641c2a61fd783a85d53df
MantasMikal <mikalauskas.it@gmail.com>
2020-07-28 20:14:20 +0100

    fix: map styles

--- web/src/components/Common/Team/index.jsx
+++ web/src/components/Common/Team/index.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { useDarkContext } from 'Context/DarkContext'
-import { cn } from 'lib/helpers'
+import React from "react";
+import PropTypes from "prop-types";
+import { useDarkContext } from "Context/DarkContext";
+import { cn } from "lib/helpers";
 
-import Map from 'Common/Map'
-import Type from 'Primitive/Type'
-import ZoomableMedia from 'Common/Zoomable'
-import Icon from 'Primitive/Icon'
-import SmartLink from 'Primitive/SmartLink'
+import Map from "Common/Map";
+import Type from "Primitive/Type";
+import ZoomableMedia from "Common/Zoomable";
+import Icon from "Primitive/Icon";
+import SmartLink from "Primitive/SmartLink";
 
-import styles from './Team.module.scss'
+import styles from "./Team.module.scss";
 
-const Team = ({ title, location, email, phone, discord, address, logo, className }) => {
-  const isDark = useDarkContext()
+const Team = ({
+  title,
+  location,
+  email,
+  phone,
+  discord,
+  address,
+  logo,
+  className,
+}) => {
+  const isDark = useDarkContext();
   return (
     <div className={cn(styles.Team, isDark && styles.isDark, className)}>
       <TeamDetails
@@ -25,26 +34,43 @@ const Team = ({ title, location, email, phone, discord, address, logo, className
         phone={phone}
       />
       <div className={styles.MapWrapper}>
-        <Map locations={[location]} center={location} mapId={`${title}-Map`} />
+        <Map
+          style={{
+            height: "100%",
+            width: "100%",
+            borderRadius: "0 3px 3px 0",
+          }}
+          locations={[location]}
+          center={location}
+          mapId={`${title}-Map`}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-const TeamDetails = ({ title, logo, address, email, phone, discord, className }) => (
+const TeamDetails = ({
+  title,
+  logo,
+  address,
+  email,
+  phone,
+  discord,
+  className,
+}) => (
   <div className={cn(styles.TeamDetails, className)}>
     <div className={styles.Logo}>
       <ZoomableMedia media={logo} />
     </div>
-    <Type size='titleMedium' className={styles.TeamTitle}>
+    <Type size="titleMedium" className={styles.TeamTitle}>
       {title}
     </Type>
-    <Type size='base' className={styles.TeamDescription}>
+    <Type size="base" className={styles.TeamDescription}>
       {address}
     </Type>
   </div>
-)
+);
 
-Team.propTypes = {}
+Team.propTypes = {};
 
-export default Team
+export default Team;
--- web/src/components/Section/ContactSection/index.jsx
+++ web/src/components/Section/ContactSection/index.jsx
@@ -1,41 +1,50 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { cn } from 'lib/helpers'
-import { useDarkContext } from 'Context/DarkContext'
+import React from "react";
+import PropTypes from "prop-types";
+import { cn } from "lib/helpers";
+import { useDarkContext } from "Context/DarkContext";
 
-import BlockContent from '../../block-content'
-import Map from 'Common/Map'
-import Container from 'Primitive/Container'
-import Type from 'Primitive/Type'
+import BlockContent from "../../block-content";
+import Map from "Common/Map";
+import Container from "Primitive/Container";
+import Type from "Primitive/Type";
 
-import styles from './ContactSection.module.scss'
+import styles from "./ContactSection.module.scss";
 
-const createVenue = _rawVenue => (
+const createVenue = (_rawVenue) => (
   <div className={styles.Venue} key={_rawVenue._key}>
     <div className={styles.BlockWrapper}>
       <BlockContent blocks={_rawVenue.body} />
     </div>
     <div className={styles.MapWrapper}>
       {_rawVenue.location && (
-        <Map center={_rawVenue.location} mpaId={_rawVenue._key} locations={[_rawVenue.location]} />
+        <Map
+          style={{
+            height: "100%",
+            width: "100%",
+            borderRadius: "0 3px 3px 0",
+          }}
+          center={_rawVenue.location}
+          mpaId={_rawVenue._key}
+          locations={[_rawVenue.location]}
+        />
       )}
     </div>
   </div>
-)
+);
 
 const ContactSection = ({ body, venues, title }) => {
-  const isDark = useDarkContext()
+  const isDark = useDarkContext();
   return (
     <Container
       className={cn(isDark && styles.isDark)}
-      size='wide'
+      size="wide"
       center
       gutter
       spacious
       withNavSpace
-      as='section'
+      as="section"
     >
-      <Type as='h1' size='displayLarge' className={styles.Title}>
+      <Type as="h1" size="displayLarge" className={styles.Title}>
         {title}
       </Type>
       {body && (
@@ -43,11 +52,13 @@ const ContactSection = ({ body, venues, title }) => {
           <BlockContent blocks={body} />
         </div>
       )}
-      <div className={styles.VenueWrapper}>{venues && venues.map(venue => createVenue(venue))}</div>
+      <div className={styles.VenueWrapper}>
+        {venues && venues.map((venue) => createVenue(venue))}
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-ContactSection.propTypes = {}
+ContactSection.propTypes = {};
 
-export default ContactSection
+export default ContactSection;
