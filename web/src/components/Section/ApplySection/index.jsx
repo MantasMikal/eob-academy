03524a0409127abf12051484178cbf987b5afed4
MantasMikal <mikalauskas.it@gmail.com>
2021-02-15 16:02:35 +0000

    fix: form redirection

--- web/src/components/Section/ApplySection/index.jsx
+++ web/src/components/Section/ApplySection/index.jsx
@@ -25,27 +25,7 @@ const venues = ["Online", "Bracknell", "Letchworth"];
 
 const Index = ({ title, blocks }) => {
   const isDark = useDarkContext();
-  const [venue, setVenue] = useState(venues[0]);
-
-  const onSubmit = (e) => {
-    function encode(data) {
-      return Object.keys(data)
-        .map(
-          (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-        )
-        .join("&");
-    }
-
-    e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": venue,
-        "name": 'Just a test'
-      }),
-    });
-  };
+  const [selectedVenue, setVenue] = useState(venues[0]);
 
   return (
     <Container
@@ -66,40 +46,8 @@ const Index = ({ title, blocks }) => {
             <BlockContent blocks={blocks} />
           </div>
         )}
-        <form
-          name={`contact-${venue}`}
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <input type="hidden" name="form-name" value={`contact-${venue}`} />
-          <FieldTemplate label="Name" required controlName="name">
-            <TextControl name="name" type="text" required />
-          </FieldTemplate>
-          <FieldTemplate label="Email" required controlName="email">
-            <TextControl name="email" type="email" required />
-          </FieldTemplate>
-          <FieldTemplate label="Phone number" controlName="phone">
-            <TextControl name="phone" type="tel" />
-          </FieldTemplate>
 
-          <FieldTemplate
-            template="multiText"
-            label="Course of interest"
-            required
-            controlName="courseOfInterest"
-          >
-            <label>
-              <VisuallyHidden>Course of interest</VisuallyHidden>
-              <SelectControl name="courseOfInterest">
-                {courses.map((course) => (
-                  <option value={course} key={`course${course}`}>
-                    {course}
-                  </option>
-                ))}
-              </SelectControl>
-            </label>
-          </FieldTemplate>
+        <div className={styles.AreaOfInterest}>
           <FieldTemplate
             template="multiText"
             label="Area of Interest"
@@ -120,26 +68,74 @@ const Index = ({ title, blocks }) => {
               </SelectControl>
             </label>
           </FieldTemplate>
-          <FieldTemplate
-            label="Message"
-            status="success"
-            required
-            controlName="message"
-          >
-            <TextControl
-              name="message"
-              placeholder="Hi!"
-              multiLine
-              rows={10}
-              required
-            />
-          </FieldTemplate>
-          <ButtonStandard className={styles.ApplyButton} onClick={onSubmit}>
-            <Type size="base" demi>
-              Apply
-            </Type>
-          </ButtonStandard>
-        </form>
+        </div>
+
+        {venues.map((venue) => {
+          return (
+            <form
+              className={cn(
+                styles.Form,
+                selectedVenue === venue && styles.visible
+              )}
+              name={`contact-${venue}`}
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+            >
+              <input
+                type="hidden"
+                name="form-name"
+                value={`contact-${venue}`}
+              />
+              <FieldTemplate label="Name" required controlName="name">
+                <TextControl name="name" type="text" required />
+              </FieldTemplate>
+              <FieldTemplate label="Email" required controlName="email">
+                <TextControl name="email" type="email" required />
+              </FieldTemplate>
+              <FieldTemplate label="Phone number" controlName="phone">
+                <TextControl name="phone" type="tel" />
+              </FieldTemplate>
+              <FieldTemplate
+                template="multiText"
+                label="Course of interest"
+                required
+                controlName="courseOfInterest"
+              >
+                <label>
+                  <VisuallyHidden>Course of interest</VisuallyHidden>
+                  <SelectControl name="courseOfInterest">
+                    {courses.map((course) => (
+                      <option value={course} key={`course${course}`}>
+                        {course}
+                      </option>
+                    ))}
+                  </SelectControl>
+                </label>
+              </FieldTemplate>
+
+              <FieldTemplate
+                label="Message"
+                status="success"
+                required
+                controlName="message"
+              >
+                <TextControl
+                  name="message"
+                  placeholder="Hi!"
+                  multiLine
+                  rows={10}
+                  required
+                />
+              </FieldTemplate>
+              <ButtonStandard className={styles.ApplyButton} type="submit">
+                <Type size="base" demi>
+                  Apply
+                </Type>
+              </ButtonStandard>
+            </form>
+          );
+        })}
       </div>
     </Container>
   );
