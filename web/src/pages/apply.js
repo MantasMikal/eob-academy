827e7d4453f2ae8ba6880580f6037e690cfa7dac
MantasMikal <mantas.codes@gmail.com>
2021-09-14 18:31:19 +0300

    fix: update script

--- web/src/pages/apply.js
+++ web/src/pages/apply.js
@@ -4,6 +4,8 @@ import { graphql } from 'gatsby'
 import SEO from '../components/seo'
 import Layout from '../containers/layout'
 import ApplySection from 'Section/ApplySection'
+import { Helmet } from 'react-helmet'
+import useScript from '../components/Hooks/useScript'
 
 export const query = graphql`
   query ApplyPageQuery {
@@ -18,11 +20,21 @@ export const query = graphql`
 const ApplyPage = props => {
   const blocks = props.data && props.data.page._rawBody
   const title = props.data && props.data.page.title
+  useScript(`hbspt.forms.create({
+    region: "eu1",
+    portalId: "24888008",
+    formId: "76b72dd9-bd61-4614-82fb-e42421c72f03"
+    });`)
   return (
+    <>
+    <Helmet>
+      <script charset="utf-8" type="text/javascript" src="https://js-eu1.hsforms.net/forms/shell.js" />
+    </Helmet>
     <Layout>
       <SEO title={title} slug='/apply' />
       <ApplySection title={title} blocks={blocks} />
     </Layout>
+    </>
   )
 }
 
