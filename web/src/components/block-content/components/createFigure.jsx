c1938d0494a8e9a612cb4c5c48930cb275a9a13a
MantasMikal <mantas.codes@gmail.com>
2021-10-20 22:24:41 +0300

    fix: home page image size

--- web/src/components/block-content/components/createFigure.jsx
+++ web/src/components/block-content/components/createFigure.jsx
@@ -1,36 +1,49 @@
-import React from 'react'
-import cfg from '../../../../config'
+import React from "react";
+import cfg from "../../../../config";
 
-import ZoomableImage from 'Common/Zoomable'
-import Media from 'Common/Media'
-import { getFluidGatsbyImage } from 'gatsby-source-sanity'
+import ZoomableImage from "Common/Zoomable";
+import Media from "Common/Media";
+import { getFluidGatsbyImage } from "gatsby-source-sanity";
 
-const createFigure = figure => {
-  if (!figure || !figure.asset || !figure.asset.mimeType) return null
-  const { isZoomable, asset, alt, maxWidth } = figure
-  
-  if (asset.mimeType === 'image/gif') {
-    return <img src={asset.url} alt={alt || ' '} style={{ width: '100%' }} key={figure.asset.id} />
+const createFigure = (figure) => {
+  if (!figure || !figure.asset || !figure.asset.mimeType) return null;
+  const { isZoomable, asset, alt, maxWidth } = figure;
+
+  if (asset.mimeType === "image/gif") {
+    return (
+      <img
+        src={asset.url}
+        alt={alt || " "}
+        style={{ width: "100%" }}
+        key={figure.asset.id}
+      />
+    );
   } else {
     const mediaProps = getFluidGatsbyImage(
       asset._id,
       { maxWidth: maxWidth || 800 },
       cfg.project
-    )
+    );
     const media = {
       asset: {
-        fluid: mediaProps
+        fluid: mediaProps,
       },
-      alt: alt || ' '
-    }
+      alt: alt || " ",
+    };
 
     const El = isZoomable === undefined || isZoomable ? ZoomableImage : Media
     return (
-      <div style={{padding: '10px 0 10px', maxWidth: maxWidth || 'auto'}}>
+      <div
+        style={{
+          padding: "10px 0 10px",
+          maxWidth: maxWidth || "auto",
+          ...(maxWidth && { margin: "auto" }),
+        }}
+      >
         <El key={figure._key} media={media} />
       </div>
-    )
+    );
   }
-}
+};
 
-export default createFigure
+export default createFigure;
