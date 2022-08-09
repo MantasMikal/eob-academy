2ad6d621945c932c2fd89496c0e5046268d0773b
MantasMikal <mantas.codes@gmail.com>
2022-08-09 22:11:23 +0300

    feat: add gtag

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -2,14 +2,15 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import GlobalMeta from '@/components/Meta/Global'
 import SmartLink from '@/components/Primitive/SmartLink'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const isPreview = pageProps?.preview === true
   return (
     <>
       {isPreview && (
-        <div className="z-10 fixed bottom-0 w-full bg-blue-600">
-          <div className="container-lg text-sm py-1 text-white">
+        <div className="fixed bottom-0 z-10 w-full bg-blue-600">
+          <div className="py-1 text-sm text-white container-lg">
             You&apos;re watching a preview.{' '}
             <SmartLink
               prefetch={false}
@@ -21,6 +22,22 @@ function MyApp({ Component, pageProps }: AppProps) {
           </div>
         </div>
       )}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-JYZX6M1Y83"
+      />
+      <Script
+        id="gtm"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JYZX6M1Y83');
+          `
+        }}
+      />
       <GlobalMeta />
       <Component {...pageProps} />
     </>
