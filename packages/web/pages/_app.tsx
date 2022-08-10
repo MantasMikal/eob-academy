9476f8e943b3157ea33f19160165ae9448cb3176
MantasMikal <mantas.codes@gmail.com>
2022-08-10 10:13:31 +0300

    fix: scripts

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import GlobalMeta from '@/components/Meta/Global'
 import SmartLink from '@/components/Primitive/SmartLink'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const isPreview = pageProps?.preview === true
@@ -21,6 +22,41 @@ function MyApp({ Component, pageProps }: AppProps) {
           </div>
         </div>
       )}
+      <Script
+        async
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-JYZX6M1Y83"
+      ></Script>
+      <Script
+        id="gtm"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JYZX6M1Y83');
+          `
+        }}
+      ></Script>
+      <Script
+        id="crisp"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.$crisp = [];
+        window.CRISP_WEBSITE_ID = "b1b7951c-3249-476c-b56f-bf42e4cfeb51";
+    
+        (function() {
+          var d = document;
+          var s = d.createElement("script");
+          s.src = "https://client.crisp.chat/l.js";
+          s.async = 1;
+          d.getElementsByTagName("head")[0].appendChild(s);
+        })();
+        `
+        }}
+      ></Script>
       <GlobalMeta />
       <Component {...pageProps} />
     </>
--- packages/web/pages/_document.tsx
+++ packages/web/pages/_document.tsx
@@ -1,6 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
-
 export default function Document() {
   return (
     <Html>
@@ -15,41 +13,6 @@ export default function Document() {
         href="https://fonts.googleapis.com/css2?family=Lexend:wght@200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet"
       />
-      <Script
-        async
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-JYZX6M1Y83"
-      ></Script>
-      <Script
-        id="gtm"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-JYZX6M1Y83');
-          `
-        }}
-      ></Script>
-      <Script
-        id="crisp"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-        window.$crisp = [];
-        window.CRISP_WEBSITE_ID = "b1b7951c-3249-476c-b56f-bf42e4cfeb51";
-    
-        (function() {
-          var d = document;
-          var s = d.createElement("script");
-          s.src = "https://client.crisp.chat/l.js";
-          s.async = 1;
-          d.getElementsByTagName("head")[0].appendChild(s);
-        })();
-        `
-        }}
-      ></Script>
       <body>
         <Main />
         <NextScript />
