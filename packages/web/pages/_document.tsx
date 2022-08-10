dc476fa9f999abe777fe4e5f0f16291e362ec3bd
MantasMikal <mantas.codes@gmail.com>
2022-08-10 10:06:50 +0300

    feat: adds crisp

--- packages/web/pages/_document.tsx
+++ packages/web/pages/_document.tsx
@@ -32,6 +32,24 @@ export default function Document() {
           `
         }}
       ></Script>
+      <Script
+        id="crisp"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.$crisp = [];
+        window.CRISP_WEBSITE_ID = "b1b7951c-3249-476c-b56f-bf42e4cfeb51";
+    
+        (function() {
+          var d = document;
+          var s = d.createElement("script");
+          s.src = "https://client.crisp.chat/l.js";
+          s.async = 1;
+          d.getElementsByTagName("head")[0].appendChild(s);
+        })();
+        `
+        }}
+      ></Script>
       <body>
         <Main />
         <NextScript />
