b78e34255367f71cf00e406b6716610a9f8e6ccb
MantasMikal <mantas.codes@gmail.com>
2022-08-29 12:37:07 +0300

    feat: add sw

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
 const withPWA = require('next-pwa')
-const withPlugins = require('next-compose-plugins')
-const isDev = process.env.NODE_ENV === 'development'
+// const withPlugins = require('next-compose-plugins')
+// const isDev = process.env.NODE_ENV === 'development'
 const { fetchSanityRedirects } = require('./services/fetch-redirects')
-const runtimeCaching = require('next-pwa/cache')
+// const runtimeCaching = require('next-pwa/cache')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
 
 const nextConfig = {
   async redirects() {
@@ -42,46 +45,48 @@ const nextConfig = {
       ]
     })
     config.resolve.alias['@'] = path.join(__dirname, './')
+    config.exclude
     return config
   }
 }
 
-module.exports = withPlugins(
-  [
-    [
-      {
-        exclude: path.resolve(__dirname, 'assets/svg'),
-        inlineImageLimit: 1
-      }
-    ],
-    [
-      withPWA,
-      {
-        pwa: {
-          register: true,
-          skipWaiting: true,
-          dest: 'public',
-          runtimeCaching,
-          // disable: process.env.NODE_ENV === 'development',
-          buildExcludes: [/middleware-manifest.json$/]
-        }
-      }
-    ],
-    ...(isDev
-      ? [
-          require('@next/bundle-analyzer')({
-            enabled: process.env.ANALYZE === 'true'
-          })
-        ]
-      : [])
-    // (nextConfig) =>
-    //   withSentryConfig(nextConfig, {
-    //     silent: true,
-    //     environment: process.env.NODE_ENV,
-    //     token: process.env.SENTRY_AUTH_TOKEN
-    //   })
-  ],
-  nextConfig
+module.exports = withBundleAnalyzer(
+  withPWA(nextConfig, {
+    pwa: {
+      dest: 'public'
+    }
+  })
 )
 
-module.exports = nextConfig
+// module.exports = withPlugins(
+//   [
+//     [
+//       {
+//         exclude: path.resolve(__dirname, 'assets/svg'),
+//         inlineImageLimit: 1
+//       }
+//     ],
+//     [
+//       withPWA,
+//       {
+//         pwa: {
+//           register: true,
+//           skipWaiting: true,
+//           dest: 'public',
+//           runtimeCaching,
+//           // disable: process.env.NODE_ENV === 'development',
+//           buildExcludes: [/middleware-manifest.json$/]
+//         }
+//       }
+//     ],
+//     // (nextConfig) =>
+//     //   withSentryConfig(nextConfig, {
+//     //     silent: true,
+//     //     environment: process.env.NODE_ENV,
+//     //     token: process.env.SENTRY_AUTH_TOKEN
+//     //   })
+//   ],
+//   nextConfig
+// )
+
+// module.exports = nextConfig
