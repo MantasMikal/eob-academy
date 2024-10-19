74b5474960684046ed4b15a02c63bf8159ff9248
MantasMikal <mantas.codes@gmail.com>
2024-10-19 14:09:14 +0300

    fix: remove department description

--- packages/studio/schemas/documents/department.js
+++ packages/studio/schemas/documents/department.js
@@ -15,11 +15,6 @@ export default {
       type: "string",
     },
     {
-      name: "description",
-      title: "Description",
-      type: "text",
-    },
-    {
       name: "teamMembers",
       title: "Team members",
       type: "array",
