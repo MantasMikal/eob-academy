0fd5ed8c864e96f8f343dbf07d537c7dfb8fd7f1
MantasMikal <mantas.codes@gmail.com>
2024-10-19 13:41:30 +0300

    fix: grid styles

--- packages/web/components/Primitive/BlockContent/components/createGrid.jsx
+++ packages/web/components/Primitive/BlockContent/components/createGrid.jsx
@@ -6,38 +6,17 @@ export function createGrid(component) {
   const gridMedia = component.gridMedia
   if (!gridMedia) return <> </>
 
-  const colTemplate = {
-    gridTemplateColumns: `${
-      component.colTemplate || 'repeat(auto-fit, minmax(250px, 1fr)'
-    }`
+  const styles = {
+    gridTemplateColumns: `${component.colTemplate || '1fr 1fr'}`,
+    gridTemplateRows: `${component.rowTemplate}`,
+    gridRowGap: `${component.rowGap || '1em'}`,
+    gridColumnGap: `${component.colGap || '1em'}`,
+    margin: component.margin,
+    marginBottom: '10px'
   }
-
-  const rowTemplate = component.rowTemplate && {
-    gridTemplateRows: `${component.rowTemplate}`
-  }
-
-  const rowGap = {
-    gridRowGap: `${component.rowGap || '1em'}`
-  }
-  const colGap = {
-    gridColumnGap: `${component.colGap || '1em'}`
-  }
-  const margin = component.margin && { margin: component.margin }
-  // const centered = component.centered && component.centered
 
   return (
-    <div
-      className="grid"
-      key={component._key + 'grid'}
-      style={{
-        ...colTemplate,
-        ...rowTemplate,
-        ...rowGap,
-        ...colGap,
-        ...margin,
-        marginBottom: '10px'
-      }}
-    >
+    <div className="grid max-sm:!grid-cols-1" key={component._key + 'grid'} style={styles}>
       {gridMedia.map((item) => (
         <div className="prose max-w-full !m-0" key={item._key}>
           {createMedia(item)}
